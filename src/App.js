db162f6f09994407e04a0d77bf4fc6feb1279cf9
Alisher Sayed <bumble469@gmail.com>
2024-05-16 23:06:12 +0530

    second update

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import './App.css';
 import Navbar from "./components/Navbar";
 import Overview from "./components/Overview";
 import Curriculum from './components/Curriculum';
+import Refund from './components/Refund';
 import Testimonials from './components/Testimonials';
+
 function App() {
   return (
   <div className="App">
     <Navbar/>
     <Overview/>
     <Curriculum/>
+    <Refund/>
     <Testimonials/>
   </div>
   );
--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -1,58 +1,42 @@
 import React from 'react'
 import "../styling/Testimonials.css"
-import clock from "../assets/clock.png"
-import cool from "../assets/coolemoji.png"
-import arrow from '../assets/right-arrow.png'
-import number2 from '../assets/number2.png'
-import hundredperc from '../assets/100-percent.png'
-import enroll from '../assets/enroll.png'
+import certificate from "../assets/certificatedemo.png"
+import star from "../assets/pinkstar.png"
+import internshipopportunities from "../assets/internship_opportunites.jpg"
 
-export default function Testimonials() {
+export default function More() {
   return (
-    <div className="testimonials">
-        <div className="rowt1">
-            <h2>Don't miss out on this <span>limited-time opportunity</span> to learn for<span> Free!</span></h2>
-            <div className="slide-bar">
-                <div className="slide-bar-row1"><h3>100% Refund Offer<img src={cool}></img></h3></div>
-                <div className="slide-bar-row2">
-                    <p>20 seats left</p>
-                    <input type="range" min="0" max="100" value="25" className="slider" />
-                </div>
-                <div className="slide-bar-row3"><p><img src={clock}></img> Offer ends in 10:00Mins</p></div>
-            </div>
+    <div className="testimonials" id="testimonials">
+      <div className="part1">
+        <div className='part1-heading'><h2>Top Companies Hiring <span>Data Scientists</span></h2></div>
+        <div className='part1-blocks'>
+          <div className='block'><p>Logo</p></div>
+          <div className='block'><p>Logo</p></div>
+          <div className='block'><p>Logo</p></div>
+          <div className='block'><p>Logo</p></div>
+          <div className='block'><p>Logo</p></div>
         </div>
-        <div className="rowt2">
-            <div className="rowt2-row1">
-                <h2>How does it work?</h2>
-            </div>
-            <div className="rowt2-row2">
-                <hr></hr>
-                <div class="image-container">
-                    <img src={arrow} alt="image1" class="start-image" />
-                    <img src={arrow} alt="image2" class="middle-image" />
-                    <img src={arrow} alt="image3" class="end-image" />
-                </div>
-            </div>
+      </div>
+      <div className='part2'>
+        <div className='part2-heading'>
+          <h2>Unlock <span>6 Certificates</span> & <span>Internship Opportunities!</span></h2>
         </div>
-        <div className="rowt3">
-            <div className="rowt3-row">
-                <div class="block">
-                    <div class="component1"><h3>Step 1 : Enroll into your favorite course for only &#8377;1,499</h3></div>
-                    <div class="component2s"><img src={enroll}/></div>
-                    <div class="component3"><p>Start Learning with <b>Lifetime course access.</b></p></div>
-                </div>
-                <div class="block">
-                    <div class="component1"><h3>Step 2 : Complete Course & Assignments within 2 years!</h3></div>
-                    <div class="component2"><img src={number2}/><p>years time from the date of enrollment</p></div>
-                    <div class="component3"><p>Finish the course within 2 years to qualify for refund.</p></div>
-                </div>
-                <div class="block">
-                    <div class="component1"><h3>Step 3 : Recieve 100% Refund upon Completion</h3></div>
-                    <div class="component2"><img src={hundredperc}/><p>Enrollment fee is refunded.</p></div>
-                    <div class="component3"><p>Upon course completion within <b>2 Years</b>, Get your <b>&#8377;1,499 Back.</b></p></div>
-                </div>
+        <div className='part2-block'>
+          <div className='block1'>
+            <h3>Get 6 Industry Recognized Certificates!</h3>
+            <img className='certificateimg' src={certificate} />
+            <div className='block1-row3'>
+              <div><img src={star}/><p>Official and Verified</p></div>
+              <div><img src={star}/><p>Enhances Credibility</p></div>
             </div>
+          </div>
+          <div className='block2'>
+            <h3>Big Internship Opportunities!</h3>
+            <p>With every course, we make you not only industry-ready but also help you crack your first internship</p>
+            <img src={internshipopportunities}/>
+          </div>
         </div>
+      </div>
     </div>
   )
 }
